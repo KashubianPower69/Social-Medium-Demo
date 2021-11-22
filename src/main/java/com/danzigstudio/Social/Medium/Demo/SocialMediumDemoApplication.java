40e60dd429069952ad2003ed60e7d1ee556a9567
ASadlek <andre.sadlek@gmail.com>
2021-11-22 19:40:15 +0100

    Initial commit

---
+++ src/main/java/com/danzigstudio/Social/Medium/Demo/SocialMediumDemoApplication.java
@@ -0,0 +1,13 @@
+package com.danzigstudio.Social.Medium.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediumDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediumDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danzigstudio/Social/Medium/Demo/SocialMediumDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danzigstudio.Social.Medium.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediumDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
